2bc59482f0e2c77ebc88e9a218b1d1adad29eaba
qian-27 <qian_27@hotmail.com>
2023-04-02 21:35:18 +0300

    Update

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import NotFound from './components/NotFound';
+
+function App() {
+  return (
+    <BrowserRouter id="textStyle">
+        <div className="App">
+          <header className="App-header">
+            <h1>Welcome to React Router</h1>
+          </header>
+        </div>
+        <nav>
+          <Link to="/">Home</Link>{' '}
+          <Link to="/about">About</Link>{' '}
+          <Link to="/contact">Contact</Link>{' '}
+        </nav>
+
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
